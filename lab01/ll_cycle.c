bdffdc7fc699d82bca088ff78d5e2b3b2f14e1ca
Zhou Ke <1600675120@qq.com>
2022-11-22 20:56:06 +0800

    Finishi lab01 2022/11/22

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,19 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *hare, *tor;
+    if(head == NULL || head->next == NULL)
+    {
+	  return 0;
+    }
+    hare = head->next;
+    tor = head;
+    while(hare != NULL && hare->next != NULL && hare != tor)
+    {
+	  hare = hare->next->next;
+	  tor = tor->next;
+    }
+    if(hare == tor)
+	  return 1;  
     return 0;
 }
