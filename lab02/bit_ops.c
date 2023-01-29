24047395a00195e0f51075e5ff502dfdadfb357c
Zhou Ke <1600675120@qq.com>
2023-01-29 16:34:50 +0800

    2023.1.29 finish lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,6 +9,11 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
+    int i = 1, j;
+    i = i << n;//仅有n位为1的数
+    j = i & x;
+    j = j >> n;//使j能够被返回
+    return j;
     return -1;
 }
 // Set the nth bit of the value of x to v.
@@ -17,11 +22,14 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    int i = 1 << n;
+    *x = ((*x & ~i) | (v << n));//先把n位置0，再把v移到n位和该数 或
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    *x = *x ^ (1 << n);
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,9 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
-}
+   unsigned bit0_std = 1, bit2_std = 1 << 2, bit3_std = 1 << 3, bit5_std = 1 << 5;
+   int bit_0 = *reg & bit0_std, bit_2 = (*reg & bit2_std) >> 2, bit_3 = (*reg & bit3_std) >> 3, bit_5 = (*reg & bit5_std) >> 5;
 
+   int leftmost_bit = bit_5 ^ (bit_3 ^ (bit_0 ^ bit_2));
+   *reg = (*reg >> 1) | (leftmost_bit << 15);
+}
